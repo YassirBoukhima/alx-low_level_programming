9fc674287687c492e185444aefe2ee8c1353151d
boukhimayassir <boukhimayassir3@gmail.com>
2023-03-17 04:59:10 -0700

    procramme c files

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
