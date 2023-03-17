77bda5f1d6dbf24d30edcf09772089b039423925
boukhimayassir <boukhimayassir3@gmail.com>
2023-03-17 02:11:39 -0700

    add style betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - start function
+ *
+ * function - puts
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
