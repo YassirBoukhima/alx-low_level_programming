f93d2d14f8737100bb64a094f98070dbe0a0a17a
boukhimayassir <boukhimayassir3@gmail.com>
2023-03-21 12:23:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ *print_alphabet - print alphapet
+ *
+ *return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for(alpha="a"; alpha <= "z"; alpha++)
+		_putchar(alpha);
+	return(0);
+}
